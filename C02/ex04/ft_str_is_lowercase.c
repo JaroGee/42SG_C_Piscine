cca2cf4b806599aa6055a5a690d36a884b34847d
Jaro Gee <jarogee@Jaros-MacBook-Air.local>
2025-10-21 08:00:00 +0800

    ex04

--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -6,47 +6,48 @@
 /*   By: mgee <mgee@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 05:22:03 by mgee              +#+    #+#             */
-/*   Updated: 2025/10/21 05:22:03 by mgee             ###   ########.fr       */
+/*   Updated: 2025/10/21 07:57:44 by mgee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_str_is_lowercase(char *str)
+int	ft_str_is_lowercase(char *str)
 {
-    int	i;
+	int	i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] < 'a' || str[i] > 'z')
-            return (0);
-        i++;
-    }
-    return (1);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 'a' || str[i] > 'z')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
+/*
 #include <unistd.h>
 
 int	ft_str_is_lowercase(char *str);
 
-int main(void)
-
+int	main(void)
 {
-    char    str1[] = "helloworld";
-    char    str2[] = "Hello42";
-    char    str3[] = "";
-    char    c;
+	char	str1[] = "helloworld";
+	char	str2[] = "Hello42";
+	char	str3[] = "";
+	char	c;
 
-    c = ft_str_is_lowercase(str1) + '0';
-    write(1, &c, 1);
-    write(1, "\n", 1);
+	c =	ft_str_is_lowercase(str1) + '0';
+	  write(1, &c, 1);
+	  write(1, "\n", 1);
 
-    c = ft_str_is_lowercase(str2) + '0';
-    write(1, &c, 1);
-    write(1, "\n", 1);
+	c =	ft_str_is_lowercase(str2) + '0';
+	  write(1, &c, 1);
+	  write(1, "\n", 1);
 
-    c = ft_str_is_lowercase(str3) + '0';
-    write(1, &c, 1);
-    write(1, "\n", 1);
+	c =	ft_str_is_lowercase(str3) + '0';
+	  write(1, &c, 1);
+	  write(1, "\n", 1);
 
-    return (0);
+	  return(0);
 }
+*/
