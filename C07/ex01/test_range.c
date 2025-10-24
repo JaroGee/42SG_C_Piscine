8bfec11048af1bfe56e453e3789692bd14a4e2d4
Jaro Gee <jaromelgee@gmail.com>
2025-10-25 03:06:04 +0800

    C07/ex01

--- C07/ex01/test_range.c
+++ C07/ex01/test_range.c
@@ -1,34 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   test_range.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgee <mgee@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/25 01:42:31 by mgee              +#+    #+#             */
-/*   Updated: 2025/10/25 02:09:48 by mgee             ###   ########.fr       */
+/*   Created: 2025/10/25 02:11:31 by mgee              +#+    #+#             */
+/*   Updated: 2025/10/25 02:11:31 by mgee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 
-int	*ft_range(int min, int max)
-{
-	int	size;
-	int	i;
-	int	*arr;
+int	*ft_range(int min, int max);
 
-	if (min >= max)
-		return (0);
-	size = max - min;
-	arr = (int *)malloc(sizeof(int) * size);
-	if (!arr)
+int	main(void)
+{
+	int i;
+	int *v;
+
+	v = ft_range(-2, 3);
+	if (!v)
 		return (0);
 	i = 0;
-	while (i < size)
+	while (i < 5)
 	{
-		arr[i] = min + i;
+		printf("%d ", v[i]);
 		i++;
 	}
-	return (arr);
+	printf("\n");
+	free(v);
+	return (0);
 }
