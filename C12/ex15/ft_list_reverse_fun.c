4697711101937083e2e450f20fb699c13812598e
Jaro Gee <jaromelgee@gmail.com>
2025-10-25 06:54:10 +0800

    C12 completed

--- C12/ex15/ft_list_reverse_fun.c
+++ C12/ex15/ft_list_reverse_fun.c
@@ -6,7 +6,7 @@
 /*   By: mgee <mgee@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 06:08:17 by mgee              +#+    #+#             */
-/*   Updated: 2025/10/25 06:16:24 by mgee             ###   ########.fr       */
+/*   Updated: 2025/10/25 06:53:30 by mgee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,24 @@ static t_list	*at(t_list *l, int i)
 	return (l);
 }
 
+static void	swap_data_at(t_list *head, int i, int j)
+{
+	t_list	*n1;
+	t_list	*n2;
+	void	*tmp;
+
+	n1 = at(head, i);
+	n2 = at(head, j);
+	tmp = n1->data;
+	n1->data = n2->data;
+	n2->data = tmp;
+}
+
 void	ft_list_reverse_fun(t_list *begin_list)
 {
 	int		size;
 	int		i;
 	t_list	*cur;
-	t_list	*n1;
-	t_list	*n2;
-	void	*tmp;
 
 	size = 0;
 	cur = begin_list;
@@ -38,11 +48,7 @@ void	ft_list_reverse_fun(t_list *begin_list)
 	i = 0;
 	while (i < size / 2)
 	{
-		n1 = at(begin_list, i);
-		n2 = at(begin_list, size - 1 - i);
-		tmp = n1->data;
-		n1->data = n2->data;
-		n2->data = tmp;
+		swap_data_at(begin_list, i, size - 1 - i);
 		i++;
 	}
 }
