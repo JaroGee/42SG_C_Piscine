58cd8404faced95dd22d48a58482f043d34f4ad5
Jaro Gee <jaromelgee@gmail.com>
2025-10-25 03:05:26 +0800

    C07/ex00

--- C07/ex00/test_strdup.c
+++ C07/ex00/test_strdup.c
@@ -1,43 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   test_strdup.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgee <mgee@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/25 01:42:30 by mgee              +#+    #+#             */
-/*   Updated: 2025/10/25 02:03:46 by mgee             ###   ########.fr       */
+/*   Created: 2025/10/25 02:10:34 by mgee              +#+    #+#             */
+/*   Updated: 2025/10/25 02:10:34 by mgee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 
-static int	ft_strlen(char *s)
+char	*ft_strdup(char *src);
+
+int	main(void)
 {
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(char *src)
-{
-	int		len;
-	int		i;
-	char	*dup;
-
-	len = ft_strlen(src);
-	dup = (char *)malloc((len + 1) * sizeof(char));
-	if (!dup)
-		return (0);
-	i = 0;
-	while (i < len)
-	{
-		dup[i] = src[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
+	char *a = ft_strdup("hello");
+	char *b = ft_strdup("");
+	printf("[%s]\n[%s]\n", a, b);
+	free(a);
+	free(b);
+	return (0);
 }
