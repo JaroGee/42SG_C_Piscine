da43a94162f649bc1883f18d21e25182ff90b6e3
Jaro Gee <jaromelgee@gmail.com>
2025-10-27 17:10:19 +0800

    fixed norrm

--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -6,54 +6,60 @@
 /*   By: mgee <mgee@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 01:42:52 by mgee              +#+    #+#             */
-/*   Updated: 2025/10/27 03:00:14 by mgee             ###   ########.fr       */
+/*   Updated: 2025/10/27 17:09:00 by mgee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "ft_stock_str.h"
 
-static void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 static void	ft_putstr(char *s)
 {
-	int i = 0;
-	while (s[i])
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
 	{
-		ft_putchar(s[i]);
+		write(1, &s[i], 1);
 		i++;
 	}
 }
 
 static void	ft_putnbr(int n)
 {
-	long nb = n;
+	long	nb;
 
+	nb = (long)n;
 	if (nb < 0)
 	{
-		ft_putchar('-');
+		write(1, "-", 1);
 		nb = -nb;
 	}
 	if (nb >= 10)
+	{
 		ft_putnbr((int)(nb / 10));
-	ft_putchar((char)('0' + (nb % 10)));
+		ft_putnbr((int)(nb % 10));
+	}
+	else
+	{
+		nb = nb + '0';
+		write(1, &nb, 1);
+	}
 }
 
-void	ft_show_tab(struct s_stock_str *par)
+void	ft_show_tab(t_stock_str *par)
 {
-	int i = 0;
+	int	i;
 
-	while (par[i].str)
+	i = 0;
+	while (par[i].str != 0)
 	{
 		ft_putstr(par[i].str);
-		ft_putchar('\n');
+		write(1, "\n", 1);
 		ft_putnbr(par[i].size);
-		ft_putchar('\n');
+		write(1, "\n", 1);
 		ft_putstr(par[i].copy);
-		ft_putchar('\n');
+		write(1, "\n", 1);
 		i++;
 	}
 }
